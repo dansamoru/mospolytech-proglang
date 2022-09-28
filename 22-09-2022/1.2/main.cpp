a414b672e24da527e12d3d63c1d38e617d7bbbb7
Daniil Samoilov <dansamoru@ya.ru>
2022-09-28 17:18:19 +0300

    Add 22-09-2022/1.2

---
+++ 22-09-2022/1.2/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    const float pi = 3.1415926;
+    cout << "Значение константы Pi=";
+    cout << pi << "." << endl;
+    
+    int length = 25;
+    cout << "Значение целочисленной переменной: " << length << "." << endl;
+    return 0;
+}
