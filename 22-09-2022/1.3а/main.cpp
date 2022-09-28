1ac3b0697da8201e9c655ba39efe83976753ee94
Daniil Samoilov <dansamoru@ya.ru>
2022-09-28 17:18:31 +0300

    Add 22-09-2022/1.3а

---
+++ 22-09-2022/1.3а/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    const float pi = 3.1415926;
+    float R;
+    cout << "Введите радиус окружности: ";
+    cin >> R;
+    cout << "Длина окружности радиуса " << R << " равна: " << 2 * pi * R << "." << endl;
+    return 0;
+}
+
+
+
