764e792a24366edc656ed90fe226fa970246e867
Daniil Samoilov <dansamoru@ya.ru>
2022-10-03 20:05:28 +0300

    Add 29-09-2022/2.3б

---
+++ 29-09-2022/2.3б/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    cout << "Программа определения простого числа" << endl;
+    cout << "---------------------------" << endl;
+    int n, s = 0, i = 2;
+    cout << "Введите натуральное число: ";
+    cin >> n;
+    if (n == 1) {
+        cout << "1 не является простым или составным";
+    }
+    while (i <= sqrt(n)) {
+        cout << "Итерация " << i - 1 << endl;
+        if (n % i == 0) {
+            s++;
+        }
+        i++;
+    }
+    if (s == 0) {
+        cout << "Число простое!";
+        
+    } else {
+        cout << "Число составное!";
+    }
+    return 0;
+}
+
