97855e8d8aa5ee4fb0722fbb4f4026c82ba6b728
Daniil Samoilov <dansamoru@ya.ru>
2022-11-10 11:40:32 +0300

    Fix 05-10-2022/3.3б

--- 05-10-2022/3.3б/main.cpp
+++ 05-10-2022/3.3б/main.cpp
@@ -6,16 +6,14 @@ int main() {
     int n, s = 0, *arr = (int *) malloc(0);
     cout << "Введите число: ";
     cin >> n;
+    cout << "Массив: ";
     do {
         arr = (int *) realloc(arr, sizeof(int));
         arr[s] = n % 10;
         n /= 10;
-        s++;
+        cout << arr[s++] << " ";
     } while (n > 0);
-    cout << "Массив: ";
-    for (int i = 0; i < s; ++i) {
-        cout << arr[i] << " ";
-    }
+    delete[] arr;
     delete[] arr;
     return 0;
 }
